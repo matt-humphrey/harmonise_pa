a54c9d6ff0749f13038edb25a29a348144ebd630
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-07-16 16:41:30 +0800

    update formatting

--- src/harmonise_pa/config/paths.py
+++ src/harmonise_pa/config/paths.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parents[3]
 
-DATA = PROJECT_ROOT / 'data'
-OG_DATA = DATA / '0 original'
-RAW_DATA = DATA / '1 raw'
-INTERIM_DATA = DATA / '2 interim'
-PROCESSED_DATA = DATA / '3 processed'
+DATA = PROJECT_ROOT / "data"
+OG_DATA = DATA / "0 original"
+RAW_DATA = DATA / "1 raw"
+INTERIM_DATA = DATA / "2 interim"
+PROCESSED_DATA = DATA / "3 processed"

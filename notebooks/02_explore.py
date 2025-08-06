99b5b3eafc0fb9ebae6316f00b870c11fbc14184
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-08-06 10:23:17 +0800

    explore HEIGHT

--- notebooks/02_explore.py
+++ notebooks/02_explore.py
@@ -14,65 +14,22 @@ def _():
     import pointblank as pb
     import polars as pl
 
-    return Path, TypeAlias, bk, mo, pb, pl
+    return mo, pb, pl
 
 
 @app.cell
 def _():
     from harmonise_pa.config import DATASETS, RAW_DATA
-
-    return DATASETS, RAW_DATA
+    from harmonise_pa import read_all_datasets
+    return DATASETS, read_all_datasets
 
 
 @app.cell
-def _(Path, RAW_DATA, TypeAlias, bk, pl):
-    Dataset: TypeAlias = dict[str, str | list | dict[str, str]]
-
-    def read_dataset(
-        file: Dataset, data_folder: Path = RAW_DATA
-    ) -> tuple[pl.DataFrame, pl.DataFrame]:
-        "Create a wrapper around `banksia.read_sav()` to simplify process of reading data."
-        file_name = file["file"]
-        variables = file["variables"]
-        rename = file["rename"]
-        # delete = file["delete"]
-
-        input_cols = variables + list(rename.keys())
-        output_cols = variables + list(rename.values())
-
-        df, meta = bk.read_sav(data_folder / file_name, usecols=input_cols)
-
-        df = df.rename(rename).select(output_cols)
-        meta = meta.with_columns(pl.col("Variable").replace(rename))
-
-        return df, meta
-
-    def read_all_datasets(
-        files: dict[Dataset], data_folder: Path = RAW_DATA
-    ) -> tuple[pl.DataFrame, pl.DataFrame]:
-        "Read and merge all datasets."
-        lfs, metas = [], []
-        for file in files:
-            df, meta = read_dataset(files[file], data_folder)
-            lfs.append(df.lazy())
-            metas.append(meta)
-
-        combined_lf = join_lazy_frames(lfs)
-        combined_meta = (
-            pl.concat(metas, how="vertical")
-            .filter(pl.col("Variable").ne("ID"))
-            .with_columns(basename=pl.col("Variable").str.slice(5))
-        )
-
-        return combined_lf, combined_meta
-
-    def join_lazy_frames(lfs: list[pl.LazyFrame]) -> pl.LazyFrame:
-        joined_lf = lfs[0]
-        for lf in lfs[1:]:
-            joined_lf = joined_lf.join(lf, on="ID", how="left")
-        return joined_lf.collect()
-
-    return (read_all_datasets,)
+def _(DATASETS):
+    gen1 = [DATASETS[k] for k in DATASETS.keys() if k.startswith("G1")]
+    gen2 = [k for k in DATASETS.keys() if k.startswith("G2")]
+    # DATASETS["G0G1"]
+    return
 
 
 @app.cell
@@ -82,51 +39,63 @@ def _(DATASETS, read_all_datasets):
 
 
 @app.cell
+def _(df):
+    df
+    return
+
+
+@app.cell
 def _(mo):
-    mo.md(r"""### A6 - Mid-arm circumference""")
+    mo.md(r"""### HEIGHT""")
     return
 
 
 @app.cell
 def _():
-    variable = "A6"
+    variable = "HEIGHT"
     return (variable,)
 
 
 @app.cell
 def _(df, pl, variable):
-    df_a6 = df.select(pl.col(f"^.*{variable}$"))
-    df_a6
-    return (df_a6,)
+    dfx = df.select(pl.col(f"^.*{variable}$"))
+    dfx
+    return (dfx,)
 
 
 @app.cell
-def _(df_a6, pb):
-    pb.Validate(data=df_a6).col_vals_between(
-        columns=pb.everything(), left=0, right=45, na_pass=True
+def _(dfx, pb):
+    pb.Validate(data=dfx).col_vals_between(
+        columns=pb.everything(), left=10, right=200, na_pass=True
     ).interrogate()
     return
 
 
 @app.cell
-def _(df_a6, pl):
-    df_a6_new = df_a6.with_columns(pl.all().replace({-99: None, 999: None})).with_columns(
+def _(dfx, pl):
+    dfx_new = dfx.with_columns(pl.all().replace({-99: None, 999: None})).with_columns(
         pl.all().round(decimals=2, mode="half_away_from_zero")
     )
 
-    df_a6_new
-    return (df_a6_new,)
+    dfx_new
+    return (dfx_new,)
 
 
 @app.cell
-def _(df_a6_new, pb):
+def _(dfx_new, pb):
     validate = (
-        pb.Validate(data=df_a6_new)
-        .col_vals_between(columns=pb.everything(), left=0, right=50, na_pass=True)
+        pb.Validate(data=dfx_new)
+        .col_vals_between(columns=pb.everything(), left=10, right=200, na_pass=True)
         .interrogate()
     )
 
     validate
+    return (validate,)
+
+
+@app.cell
+def _(validate):
+    validate.get_step_report(i=18)
     return
 
 

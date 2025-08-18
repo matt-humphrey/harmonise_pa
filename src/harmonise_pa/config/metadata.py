95234f9f586766687e49ce864a675d874698b4c0
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-08-18 11:26:15 +0800

    include supine blood pressure variables

--- src/harmonise_pa/config/metadata.py
+++ src/harmonise_pa/config/metadata.py
@@ -248,8 +248,31 @@ BP2 = {
     "field_values": None,
 }
 
-# BP3 = ...
-# BP4 = ...
+BP3 = {
+    "basename": r"_BP3$",
+    "field_label": "Blood pressure state",
+    "field_type": "Numeric",
+    "field_width": 1,
+    "decimals": 0,
+    "var_type": "nominal",
+    "field_values": {1: "Quiet/relaxed", 2: "Excited/agitated", 3: "Crying"},
+}
+
+BP4 = {
+    "basename": r"_BP4$",
+    "field_label": "Blood pressure instrument",
+    "field_type": "Numeric",
+    "field_width": 1,
+    "decimals": 0,
+    "var_type": "nominal",
+    "field_values": {
+        1: "Mercury sphygmomanometer",
+        2: "Aneroid sphygmomanometer",
+        3: "Dinamap BP recorder",
+        4: "Other electronic device (e.g. infrasound)",
+        5: "Other (e.g. manual palpitation)",
+    },
+}
 
 BP5 = {
     "basename": r"_BP5$",
@@ -259,4 +282,410 @@ BP5 = {
     "decimals": 0,
     "var_type": "scale",
     "field_values": None,
+}
+
+
+BP46 = {
+    "basename": r"_BP46$",
+    "field_label": "Systolic blood pressure (resting/seated) - 0 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP47 = {
+    "basename": r"_BP47$",
+    "field_label": "Diastolic blood pressure (resting/seated) - 0 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP48 = {
+    "basename": r"_BP48$",
+    "field_label": "Heart rate (resting/seated) - 0 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP49 = {
+    "basename": r"_BP49$",
+    "field_label": "Systolic blood pressure (resting/seated) - 2 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP50 = {
+    "basename": r"_BP50$",
+    "field_label": "Diastolic blood pressure (resting/seated) - 2 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP51 = {
+    "basename": r"_BP51$",
+    "field_label": "Heart rate (resting/seated) - 2 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP52 = {
+    "basename": r"_BP52$",
+    "field_label": "Systolic blood pressure (resting/seated) - 4 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP53 = {
+    "basename": r"_BP53$",
+    "field_label": "Diastolic blood pressure (resting/seated) - 4 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP54 = {
+    "basename": r"_BP54$",
+    "field_label": "Heart rate (resting/seated) - 4 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP55 = {
+    "basename": r"_BP55$",
+    "field_label": "Systolic blood pressure (resting/seated) - 6 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP56 = {
+    "basename": r"_BP56$",
+    "field_label": "Diastolic blood pressure (resting/seated) - 6 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP57 = {
+    "basename": r"_BP57$",
+    "field_label": "Heart rate (resting/seated) - 6 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP58 = {
+    "basename": r"_BP58$",
+    "field_label": "Systolic blood pressure (resting/seated) - 8 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP59 = {
+    "basename": r"_BP59$",
+    "field_label": "Diastolic blood pressure (resting/seated) - 8 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP60 = {
+    "basename": r"_BP60$",
+    "field_label": "Heart rate (resting/seated) - 8 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP61 = {
+    "basename": r"_BP61$",
+    "field_label": "Systolic blood pressure (resting/seated) - 10 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP62 = {
+    "basename": r"_BP62$",
+    "field_label": "Diastolic blood pressure (resting/seated) - 10 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP63 = {
+    "basename": r"_BP63$",
+    "field_label": "Heart rate (resting/seated) - 10 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP64 = {
+    "basename": r"_BP64$",
+    "field_label": "Systolic blood pressure (supine) - 0 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP65 = {
+    "basename": r"_BP65$",
+    "field_label": "Diastolic blood pressure (supine) - 0 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP66 = {
+    "basename": r"_BP66$",
+    "field_label": "Heart rate (supine) - 0 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP67 = {
+    "basename": r"_BP67$",
+    "field_label": "Systolic blood pressure (supine) - 2 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP68 = {
+    "basename": r"_BP68$",
+    "field_label": "Diastolic blood pressure (supine) - 2 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP69 = {
+    "basename": r"_BP69$",
+    "field_label": "Heart rate (supine) - 2 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP70 = {
+    "basename": r"_BP70$",
+    "field_label": "Systolic blood pressure (supine) - 4 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP71 = {
+    "basename": r"_BP71$",
+    "field_label": "Diastolic blood pressure (supine) - 4 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP72 = {
+    "basename": r"_BP72$",
+    "field_label": "Heart rate (supine) - 4 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP73 = {
+    "basename": r"_BP73$",
+    "field_label": "Systolic blood pressure (supine) - 6 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP74 = {
+    "basename": r"_BP74$",
+    "field_label": "Diastolic blood pressure (supine) - 6 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP75 = {
+    "basename": r"_BP75$",
+    "field_label": "Heart rate (supine) - 6 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP76 = {
+    "basename": r"_BP76$",
+    "field_label": "Systolic blood pressure (supine) - 8 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP77 = {
+    "basename": r"_BP77$",
+    "field_label": "Diastolic blood pressure (supine) - 8 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP78 = {
+    "basename": r"_BP78$",
+    "field_label": "Heart rate (supine) - 8 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP79 = {
+    "basename": r"_BP79$",
+    "field_label": "Systolic blood pressure (supine) - 10 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP80 = {
+    "basename": r"_BP80$",
+    "field_label": "Diastolic blood pressure (supine) - 10 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP81 = {
+    "basename": r"_BP81$",
+    "field_label": "Heart rate (supine) - 10 mins",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BPCD = {
+    "basename": r"_BPCD$",
+    "field_label": "Blood pressure code",
+    "field_type": "Numeric",
+    "field_width": 1,
+    "decimals": 0,
+    "var_type": "nominal",
+    "field_values": {
+        0: "No abnormality detected",
+        1: "Pregnant",
+        2: "Increased blood pressure (hypertensive)",
+    },
+}
+
+BP_TEMP = {
+    "basename": r"_BP_TEMP$",
+    "field_label": "Temperature of assessment room (C)",
+    "field_type": "Numeric",
+    "field_width": 3,
+    "decimals": 1,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+# TODO: what type/width?
+BP_TIME = {
+    "basename": r"_BP_TIME$",
+    "field_label": "Time of blood pressure assessment",
+    "field_type": "Time",
+    "field_width": 8,  # HH:MM:SS
+    "decimals": 0,
+    "var_type": "scale",
+    "field_values": None,
+}
+
+BP_CUFF = {
+    "basename": r"_BP_CUFF$",
+    "field_label": "Size of cuff for blood pressure",
+    "field_type": "Numeric",
+    "field_width": 1,
+    "decimals": 0,
+    "var_type": "nominal",
+    "field_values": {1: "Small", 2: "Medium", 3: "Large", 4: "Extra large/thigh"},
 }

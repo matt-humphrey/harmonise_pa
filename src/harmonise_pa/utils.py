78a0be1f9f61135c397e74243124f8e0208b8557
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-08-26 13:03:33 +0800

    add functions for interim data and data pipeline

--- src/harmonise_pa/utils.py
+++ src/harmonise_pa/utils.py
@@ -1,25 +1,51 @@
+from functools import reduce
 from pathlib import Path
-from typing import TypeAlias
+from typing import Callable, TypeAlias
 
 import banksia as bk
 import polars as pl
+from polars import DataFrame
 
-from harmonise_pa.config import RAW_DATA
+from harmonise_pa.config import INTERIM_DATA, RAW_DATA
 
 __all__ = [
+    "apply_pipeline",
+    "make_interim_datasets",
     "read_dataset",
     "read_all_datasets",
+    "read_sav",
+    "transform_datasets",
 ]
 
 Dataset: TypeAlias = dict[str, str | list | dict[str, str]]
+Transform: TypeAlias = Callable[[DataFrame], DataFrame]
+
+
+def read_sav(file: Dataset, data_folder: Path = RAW_DATA) -> tuple[pl.DataFrame, pl.DataFrame]:
+    """
+    Create a wrapper around `banksia.read_sav()` for reading files and performing transformations
+    like renaming and deleting variables for harmonising.
+    """
+    file_name = file["file"]
+    rename = file["rename"]
+    delete = file["delete"]
+
+    df, meta = bk.read_sav(data_folder / file_name)
+
+    df = df.rename(rename).drop(delete)
+    meta = meta.with_columns(pl.col("Variable").replace(rename))
+
+    return df, meta
 
 
 def read_dataset(file: Dataset, data_folder: Path = RAW_DATA) -> tuple[pl.DataFrame, pl.DataFrame]:
-    "Create a wrapper around `banksia.read_sav()` to simplify process of reading data."
+    """
+    Create a wrapper around `banksia.read_sav()` for reading and renaming specific variables for
+    initial data exploration.
+    """
     file_name = file["file"]
     variables = file["variables"]
     rename = file["rename"]
-    # delete = file["delete"]
 
     input_cols = variables + list(rename.keys())
     output_cols = variables + list(rename.values())
@@ -61,3 +87,44 @@ def _join_lazy_frames(lfs: list[pl.LazyFrame]) -> pl.LazyFrame:
 
 def _concat_lazy_frames(lfs: list[pl.LazyFrame]) -> pl.LazyFrame:
     return pl.concat(lfs, how="align")
+
+
+def apply_pipeline(df: DataFrame, transforms: list[Transform]) -> DataFrame:
+    "Selectively apply functions in a composable pipeline"
+    return reduce(lambda result, fn: fn(result), transforms, df)
+
+
+def transform_datasets(
+    datasets: dict[str, DataFrame], pipelines: dict[str, Transform]
+) -> dict[str, DataFrame]:
+    "Apply defined transformations to the datasets"
+    return {
+        dset: apply_pipeline(df, pipelines[dset])
+        for dset, df in datasets.items()
+        if dset in pipelines
+    }
+
+
+def make_interim(file: Dataset, raw: Path = RAW_DATA) -> tuple[pl.DataFrame, pl.DataFrame]:
+    """
+    Create a wrapper around `banksia.read_sav()` for reading files and performing transformations
+    like renaming and deleting variables for harmonising.
+    """
+    file_name = file["file"]
+    rename = file["rename"]
+    delete = file["delete"]
+
+    df, meta = bk.read_sav(raw / file_name)
+
+    df = df.rename(rename).drop(delete)
+    meta = meta.with_columns(pl.col("Variable").replace(rename))
+
+    return df, meta
+
+
+def make_interim_datasets(
+    datasets: dict[str, Dataset],
+):
+    for name, dset in datasets.items():
+        df, meta = make_interim(dset, RAW_DATA)
+        bk.write_sav(INTERIM_DATA / dset["file"], df, meta)

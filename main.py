3e7ff12b845635ef17d3df4a4b5db5361084554d
Matt Humphrey <34499789+matt-humphrey@users.noreply.github.com>
2025-08-26 13:01:20 +0800

    clean up project

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from harmonise-pa!")
+import banksia as bk
+import polars as pl
 
+from harmonise_pa import (
+    DATA_TRANSFORMS,
+    read_sav,
+    transform_datasets,
+)
+from harmonise_pa.config import DATASETS, METADATA, PROCESSED_DATA, RAW_DATA
 
-if __name__ == "__main__":
-    main()
+datasets = {k: v for k, v in DATASETS.items()}
+
+dfs, metas = {}, {}
+
+for name, dset in datasets.items():
+    df, meta = read_sav(dset, RAW_DATA)
+    dfs[name] = df
+    metas[name] = meta
+
+transformed_dfs = transform_datasets(dfs, DATA_TRANSFORMS)
+
+new_metas = bk.transform_metadata(metas, METADATA)
